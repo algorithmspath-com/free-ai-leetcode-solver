0094ae3b4c822d92459cef21e40e9ac204a1fe44
Your Name <contact@hypothesisbase.com>
2025-04-16 17:17:25 -0400

    windows backend

---
+++ config.sample.py
@@ -0,0 +1,6 @@
+{
+  "openai_api_key": "my_openai_api_key",
+  "tesseract_cmd": "C:/Tesseract/tesseract.exe",
+  "host": "0.0.0.0",    # Host for the FastAPI server
+  "port": 10010          # Port for the FastAPI server
+}
